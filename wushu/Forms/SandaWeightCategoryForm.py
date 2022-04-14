3bf3070e8efc9e5732e893dec1967cdcc89e4ddb
90531 <503020200061@ogrenci.ogu.edu.tr>
2022-04-14 14:12:39 +0300

    siklet kategorisi düzenlendi

--- wushu/Forms/SandaWeightCategoryForm.py
+++ wushu/Forms/SandaWeightCategoryForm.py
@@ -7,13 +7,14 @@ from wushu.models.SandaWeightCategory import SandaWeightCategory
 class SandaWeightCategoryForm(ModelForm):
     class Meta:
         model = SandaWeightCategory
-        fields = ('categoryName', 'isDuilian', 'explanation')
-        labels = {'categoryName': 'Category Name', 'isDuilian': 'isDuilian', 'explanation': 'explanation'}
+        fields = ('categoryName', 'ageGroup', 'gender')
+        labels = {'categoryName': 'Category Name', 'ageGroup': 'Age Group', 'gender': 'Gender'}
         widgets = {
             'categoryName': forms.TextInput(
                 attrs={'class': 'form-control', }),
-            'isDuilian': forms.CheckboxInput(attrs={'class': 'iCheck-helper'}),
-            'explanation': forms.TextInput(
+            'gender': forms.TextInput(
+                attrs={'class': 'form-control', }),
+            'ageGroup': forms.TextInput(
                 attrs={'class': 'form-control '}),
 
         }
