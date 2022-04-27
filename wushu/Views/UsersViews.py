01cdb69c7414d4c13817ea46004588bcc0791068
ismailyegin <ismail@kobiltek.com>
2022-04-27 15:44:30 +0300

    bug fix

--- wushu/Views/UsersViews.py
+++ wushu/Views/UsersViews.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from oxiterp.settings.base import BASE_DIR
+from oxiterp.settings.base import BASE_DIR, MEDIA_ROOT
 from wushu.models import SandaAthlete, SandaCoach, SandaObserver, SandaOfficer, SandaJudge, TaoluAthlete, TaoluCoach, \
     TaoluObserver, TaoluOfficer, TaoluJudge, Person
 from wushu.services import general_methods
@@ -20,23 +20,23 @@ def profile_image(request):
     if not perm:
         logout(request)
         return redirect('accounts:login')
-    if not (os.path.isdir('media/profile_image')):
-        os.makedirs(os.path.join('media','profile_image'))
+    if not os.path.isdir(MEDIA_ROOT+'\\profile_image'):
+        os.makedirs(os.path.join(MEDIA_ROOT,'profile_image'))
 
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/profile_image')
+    root = os.path.join(MEDIA_ROOT, 'profile_image')
     try:
         for item in Person.objects.all()[:5]:
             if item.profileImage:
                 extension='.'+item.profileImage.name.split('.')[1]
-                dst = os.path.join(BASE_DIR, 'media/profile_image/' + item.pasaport+extension)
+                dst = os.path.join(MEDIA_ROOT, 'profile_image\\' + item.pasaport+extension)
                 if not Path(dst).is_file():
                     shutil.copyfile(item.profileImage.path, dst)
         zip_file=None
-        is_file=Path(os.path.join(BASE_DIR, 'media/profile_image.zip'))
+        is_file=Path(os.path.join(MEDIA_ROOT,'profile_image.zip'))
         if not is_file.is_file():
-            zip_file=shutil.make_archive(MEDIA_ROOT, 'zip', MEDIA_ROOT)
+            zip_file=shutil.make_archive(root, 'zip', root)
         else:
-            zip_file=os.path.join(BASE_DIR, 'media/profile_image.zip')
+            zip_file=os.path.join(MEDIA_ROOT, 'profile_image.zip')
         # file=os.path.join(BASE_DIR, 'media/profile_image.zip')
         # html='<a href="/media/profile_image.zip" download>Download the file by clicking here</a>'
         if Path(zip_file).is_file():
